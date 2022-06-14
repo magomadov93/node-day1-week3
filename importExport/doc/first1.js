f1a156483ec49a8e8c87baae30563d0617a1a65d
magomadov93 <madoldelax@mail.ru>
2022-06-14 13:26:44 +0300

    sdelal

---
+++ importExport/doc/first1.js
@@ -0,0 +1,8 @@
+let age = 5;
+let name = "Usman";
+
+const sum = (a,b)=> {
+    return a + b
+  }
+  export default {age, name, sum
+  }
---
+++ importExport/doc/second1.js
@@ -0,0 +1,4 @@
+import fff from "./first1.js";
+console.log(fff.age)
+console.log(fff.name)
+console.log(fff.sum(1, 2));
